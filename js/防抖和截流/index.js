c835cabfc2a51672f41303af1512a145501da803
yeyunwen <1571374338@qq.com>
2024-09-19 14:19:55 +0800

    feat(js): add 防抖和截流

---
+++ js/防抖和截流/index.js
@@ -0,0 +1,24 @@
+export const debounce = (fn, wait, de) => {
+  let timer = null;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, wait);
+  };
+};
+
+export const throtle = (fn, wait) => {
+  let timer = null;
+  return function (...args) {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null;
+    }, wait);
+  };
+};
