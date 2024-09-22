c8ab9882181caa9614c33eba8fd7487b4c7f4ff2
yeyunwen <1571374338@qq.com>
2024-09-22 11:44:16 +0800

    fix(test): 发布订阅单测

--- js/发布订阅模式/index.test.ts
+++ js/发布订阅模式/index.test.ts
@@ -4,39 +4,44 @@ import { EventBus } from "./index";
 describe("发布订阅模式", () => {
   const eventBus = new EventBus();
 
-  test("事件订阅", () => {
+  test("事件订阅/发布", () => {
+    let count = 0;
     const callback = () => {
+      count++;
       console.log("Callback executed");
     };
     eventBus.on("event1", callback);
-    expect(eventBus.emit("event1")).toContain(callback);
-  });
-
-  test("事件发布", () => {
-    const callback = () => {
-      console.log("Callback executed");
-    };
-    eventBus.on("event2", callback);
-    eventBus.emit("event2");
+    eventBus.emit("event1");
+    expect(count).toBe(1);
+    eventBus.emit("event1");
+    expect(count).toBe(2);
   });
 
   test("事件取消订阅", () => {
+    let count = 0;
     const callback = () => {
+      count++;
       console.log("Callback executed");
     };
     eventBus.on("event3", callback);
+    eventBus.emit("event3");
+    expect(count).toBe(1);
     eventBus.off("event3", callback);
-    expect(eventBus.emit("event3")).not.toContain(callback);
+    eventBus.emit("event3");
+    expect(count).toBe(1);
   });
 
   test("事件只执行一次", () => {
+    let count = 0;
     const callback = () => {
+      count++;
       console.log("Callback executed");
     };
     eventBus.once("event4", callback);
-    expect(eventBus.emit("event4")).toContain(callback);
+    eventBus.emit("event4");
+    expect(count).toBe(1);
     eventBus.emit("event4");
     eventBus.emit("event4");
-    expect(eventBus.emit("event4")).not.toContain(callback);
+    expect(count).toBe(1);
   });
 });
